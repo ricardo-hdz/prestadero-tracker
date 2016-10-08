4c194905d4f19ef929c25cfe2e97ed79aec0b7fa
Ricardo Hernandez <rhernande@Macintosh-456.local>
2016-10-08 11:39:23 -0700

    version1.0

--- code.js
+++ code.js
@@ -2,11 +2,10 @@ var SPREADSHEET_MAIN = 'Operaciones';
 var SPREADSHEET_MASTER = 'Master';
 var spreadsheet;
 var masterSheet;
-var defaults = getDefaults();
 
 setSheets();
 
-var ROW_START = 2;
+var ROW_START = 17;
 var OPERATIONS_RANGE = operationsSheet.getRange('D2:D200');
 var AMOUNT_RANGE = operationsSheet.getRange('F2:F200');
 var DETAIL_RANGE = operationsSheet.getRange('H2:H200');
@@ -19,9 +18,32 @@ var OPERATION_TYPES = [
   'RETIRO'
 ];
 
+var PAYMENT_TYPES = [
+  'Principal',
+  'Interes',
+  'ImpuestoInteres',
+  'Moratorios',
+  'ImpuestoMoratorios'
+];
+
+var TOTAL_MAPPINGS = {
+    'ABONO': 'B1',
+    'COMISION': 'B6',
+    'PAGOS': 'B8',
+    'FONDEO': 'B4',
+    'RETIRO': 'B2'
+};
+
+var PAYMENT_MAPPINGS = {
+    'Principal': 'B9',
+    'Interes': 'B10',
+    'ImpuestoInteres': 'B11',
+    'Moratorios': 'B12',
+    'ImpuestoMoratorios': 'B13'
+};
+
 function setSheets() {
     operationsSheet = SpreadsheetApp.getActiveSpreadsheet();
-    masterSheet = spreadsheet.getSheetByName(SPREADSHEET_MASTER);
 }
 
 
@@ -30,33 +52,84 @@ function onOpen() {
   var menuEntries = [
     {name: "Process Operations", functionName: "processOperations"}
   ];
-  spreadsheet.addMenu("Stocks To Ticks", menuEntries);
+  operationsSheet.addMenu("Process Operations", menuEntries);
+}
+
+function initTotalsByType(types) {
+    var totalsByType = {};
+    for (var i = 0, type;(type = types[i]); i++) {
+        totalsByType[type] = 0;
+    }
+    return totalsByType;
 }
 
 function processOperations() {
+    var operationType,
+        totalsByType,
+        amount;
 
+    totalsByType = initTotalsByType(OPERATION_TYPES);
+
+    for (var i = ROW_START, operation; (operation = operationsSheet.getRange('D' + i)); i++) {
+        operationType = operation.getValue();
+        if (operationType !== '') {
+            if (totalsByType[operationType] !== null) {
+                // Get amount
+                amount = operationsSheet.getRange('F' + i).getValue();
+                totalsByType[operationType] += amount;
+            }
+        } else {
+            break;
+        }
+    }
+
+    for (var j = 0; (operationType = OPERATION_TYPES[j]); j++) {
+        var total = totalsByType[operationType];
+        var range = TOTAL_MAPPINGS[operationType];
+        operationsSheet.getRange(range).setValue(total);
+    }
+
+    processPayments();
 }
 
-function processStocks() {
-    var stockName = '';
-    var tick;
-    var purchasePrice;
+// Principal: 151.2554 Interes: 33.8447 Impuesto Interes: 5.41515 Moratorios: 0 Impuesto Moratorios: 0
+function processPayments() {
+    var totalsByType,
+        detail;
+
+    totalsByType = initTotalsByType(PAYMENT_TYPES);
 
     for (var i = ROW_START, operation; (operation = operationsSheet.getRange('D' + i)); i++) {
         operationType = operation.getValue();
 
-    }
+        if (operationType !== '') {
+            // Split then process
+            if (operationType !== 'PAGOS') {
+                continue;
+            };
 
-    for (var i = 2, stock; (stock = masterSheet.getRange('B' + i)); i++) {
-        stockName = stock.getValue();
-        if (stockName !== '') {
-            tick = defaults[stockName];
-            masterSheet.getRange('F' + i).setValue(tick);
-            purchasePrice = masterSheet.getRange('D' + i).getValue() / masterSheet.getRange('E' + i).getValue();
-            masterSheet.getRange('G' + i).setValue(purchasePrice);
-            masterSheet.getRange('H' + i).setValue('Buy');
+            detail = operationsSheet.getRange('H' + i).getValue();
+            if (detail !== '') {
+                var replaceStr = detail.replace(/: /gi, ':');
+                replaceStr = replaceStr.replace(/Impuesto /gi, 'Impuesto');
+                var payments = replaceStr.split(' ');
+                for (var j = 0, payment; (payment = payments[j]); j++) {
+                    if (payment !== null && payment !== '') {
+                        var paymentData = payment.split(':');
+                        if (paymentData.length === 2) {
+                            totalsByType[paymentData[0]] += Math.abs(paymentData[1]);
+                        }
+                    }
+                }
+            }
         } else {
             break;
         }
+    }
+
+    for (var i = 0, paymentType; (paymentType = PAYMENT_TYPES[i]); i++) {
+        var total = totalsByType[paymentType];
+        var range = PAYMENT_MAPPINGS[paymentType];
+        operationsSheet.getRange(range).setValue(total);
     }
 }
